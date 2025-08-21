b2287669172761604b2c61b664d93262be3bd954
momo <googolmo@gmail.com>
2025-08-21 23:53:06 +0000

    change plugin name

--- yellowstone-grpc-geyser/src/plugin.rs
+++ yellowstone-grpc-geyser/src/plugin.rs
@@ -57,7 +57,7 @@ impl Plugin {
 
 impl GeyserPlugin for Plugin {
     fn name(&self) -> &'static str {
-        concat!(env!("CARGO_PKG_NAME"), "-", env!("CARGO_PKG_VERSION"))
+        concat!(env!("CARGO_PKG_NAME"),"-", "zero", "-", env!("CARGO_PKG_VERSION"))
     }
 
     fn on_load(&mut self, config_file: &str, is_reload: bool) -> PluginResult<()> {
